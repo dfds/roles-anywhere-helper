e661cc20f1c38a4781f0aa52ba2e5b79fbf64a45
John Smith <josmi@dfds.com>
2023-03-03 12:50:03 +0000

    remove default stuff

--- src/cmd/configure.go
+++ src/cmd/configure.go
@@ -19,13 +19,13 @@ var configureCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(configureCmd)
 
-	configureCmd.PersistentFlags().StringP(Flags.ProfileName, "n", "", "")
-	configureCmd.PersistentFlags().StringP(Flags.CertificateDirectory, "x", "", "")
-	configureCmd.PersistentFlags().StringP(Flags.PrivateKeyDirectory, "k", "", "")
-	configureCmd.PersistentFlags().StringP(Flags.TrustAnchor, "t", "", "")
-	configureCmd.PersistentFlags().StringP(Flags.ProfileArn, "p", "", "")
-	configureCmd.PersistentFlags().StringP(Flags.RoleArn, "i", "", "")
-	configureCmd.PersistentFlags().StringP(Flags.Region, "r", "", "")
+	configureCmd.PersistentFlags().StringP(Flags.ProfileName, "n", "default", "Name of the profile to that the credentials will be created under")
+	configureCmd.PersistentFlags().StringP(Flags.CertificateDirectory, "x", "", "Destination of the Certificate on the machine")
+	configureCmd.PersistentFlags().StringP(Flags.PrivateKeyDirectory, "k", "", "Destination of the unincrypted private key on the machine")
+	configureCmd.PersistentFlags().StringP(Flags.TrustAnchor, "t", "", "The Arn of the AWS IAM roles anywhere trust anchor")
+	configureCmd.PersistentFlags().StringP(Flags.ProfileArn, "p", "", "The Arn of the AWS IAM roles Anywhere profile")
+	configureCmd.PersistentFlags().StringP(Flags.RoleArn, "i", "", "The Arn of the role to be assumed with AWS IAM roles Anywhere")
+	configureCmd.PersistentFlags().StringP(Flags.Region, "r", "us-east-1", "The region for the credential profile")
 
 	cobra.MarkFlagRequired(configureCmd.PersistentFlags(), Flags.CertificateDirectory)
 	cobra.MarkFlagRequired(configureCmd.PersistentFlags(), Flags.PrivateKeyDirectory)
--- src/cmd/root.go
+++ src/cmd/root.go
@@ -12,13 +12,8 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "iam-anywhere-ninja",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI tool to help development teams with IAM roles anywhere setup.",
+	Long:  `A CLI tool for to help assist with the setup of IAM roles anywhere on on-premess machines.`,
 }
 
 func Execute() {
